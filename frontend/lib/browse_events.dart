a31857e1e9bc1e1bcf051e942a7a90b40b2bf6da
n-maido <nhi_maido@outlook.com>
2021-08-04 17:08:26 -0700

    create and navigate to blank events screen

---
+++ frontend/lib/browse_events.dart
@@ -0,0 +1,12 @@
+// Browse events screen
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'graphql_conf.dart';
+
+class BrowseEvents extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Browse Events')), body: Center());
+  }
+}
