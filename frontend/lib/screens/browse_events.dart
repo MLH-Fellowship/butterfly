b3aea713ec4c5491bad440077002b96e49943795
manlalaro1 <odonnellp46@gmail.com>
2021-08-10 16:44:55 -0400

    data being passsed to buildEventsCard

--- frontend/lib/screens/browse_events.dart
+++ frontend/lib/screens/browse_events.dart
@@ -74,18 +74,18 @@ class _EventListState extends State<EventList> {
               builder: (QueryResult? result,
                   {VoidCallback? refetch, FetchMore? fetchMore}) {
         
-                final events = (result!.data['allEvents']);
-                print(events);
+                final events = result!.data['allEvents'];
 
                 return Expanded(
                   child: ListView.builder(
                     itemCount: events.length,
                     itemBuilder: (BuildContext context, int index) {
                       final event = events[index];
-                      return ListTile(
-                        title: Text(event['name']),
-                        // subtitle: Text(event['description']),
-                      );
+                      // return ListTile(
+                      //   title: Text(event['name']),
+                      //   // subtitle: Text(event['description']),
+                      // );
+                      return buildEventCard(event);
                     },
                   ),
                 );
@@ -97,34 +97,6 @@ class _EventListState extends State<EventList> {
     );
   }
 }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Query(
-//       options: QueryOptions(
-//         documentNode:
-//             gql(getAllEvents), // this is the query string you just created
-//       ),
-//       builder: (QueryResult result,
-//           {VoidCallback? refetch, required FetchMore? fetchMore}) {
-//         // if (result.hasException) {
-//         //   return Text(result.exception.toString());
-//         // }
-
-//         // it can be either Map or List
-//         List events = result.data['allEvents'];
-
-//         return ListView.builder(
-//             itemCount: events.length,
-//             itemBuilder: (context, index) {
-//               final event = events[index];
-
-//               return Text(event['name']);
-//             });
-//       },
-//     );
-//   }
-// }
 
 // class BrowseEvents extends StatelessWidget {
 //   @override
@@ -141,87 +113,86 @@ class _EventListState extends State<EventList> {
 //         ));
 //   }
 
-// Widget buildEventCard() => Card(
-//       // make corners rounded
-//       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-//       // add drop shadow
-//       shadowColor: Colors.grey.withOpacity(0.5),
-//       elevation: 5,
-//       margin: EdgeInsets.all(12),
-//       child: InkWell( // wrap in gesture detector to make card clickable
-//         onTap: _cardTapped,
-//         borderRadius: BorderRadius.all(Radius.circular(50)),
-//         child:
-//       Container(
-//         padding: EdgeInsets.all(18),
-//         child: Row(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           buildCardLeft(
-//             eventName: 'Martha\'s Birthday Bash',
-//             location: '1234 W Sample St, Vancouver, BC',
-//             time: '6:00pm - 11:00pm',
-//             description: 'Celebrating Martha\'s 75th birthday'
-//           ),
-//           Container(
-//             padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-//             child: buildCardRight(
-//               month: 'NOV',
-//               day: '4'
-//             ),
-//           )
-//         ],)
-//       )
-//     )
-//   );
+Widget buildEventCard(event) => Card(
+      // make corners rounded
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+      // add drop shadow
+      shadowColor: Colors.grey.withOpacity(0.5),
+      elevation: 5,
+      margin: EdgeInsets.all(12),
+      child: InkWell( // wrap in gesture detector to make card clickable
+        onTap: _cardTapped,
+        borderRadius: BorderRadius.all(Radius.circular(50)),
+        child:
+      Container(
+        padding: EdgeInsets.all(18),
+        child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          buildCardLeft(
+            eventName: event['name'],
+            location: event['location'],
+            time: event['date'],
+            // description: event['description']
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: buildCardRight(
+              month: event['date'].substring(5,7),
+              day: event['date'].substring(8,10),
+            ),
+          )
+        ],)
+      )
+    )
+  );
 
-//     // card text in left col
-//     Widget buildCardLeft({required String eventName, required String location, required String time, required String description }) {
-//       print(eventName);
-//       return Column(
-//       crossAxisAlignment: CrossAxisAlignment.start, //left-aligned
-//       children: [
-//         Container(
-//           child: Text(eventName, style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),)
-//         ),
-//         Container(
-//           padding: EdgeInsets.fromLTRB(0, 4, 0, 0),
-//           child: Text(location, style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300, fontStyle: FontStyle.italic)
-//           )
-//         ),
-//         Container(
-//           padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
-//           child: Text(time, style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300, fontStyle: FontStyle.italic)
-//           )
-//         ),
-//         Container(
-//           padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
-//           child: Text(description, style: TextStyle(fontSize: 15), overflow: TextOverflow.ellipsis)
-//         )
-//       ]
-//     );
-//   }
+    // card text in left col
+    Widget buildCardLeft({required String eventName, required String location, required String time}) {
+      print(eventName);
+      return Column(
+      crossAxisAlignment: CrossAxisAlignment.start, //left-aligned
+      children: [
+        Container(
+          child: Text(eventName, style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),)
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(0, 4, 0, 0),
+          child: Text(location, style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300, fontStyle: FontStyle.italic)
+          )
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
+          child: Text(time, style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300, fontStyle: FontStyle.italic)
+          )
+        ),
+        // Container(
+        //   padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
+        //   child: Text(description, style: TextStyle(fontSize: 15), overflow: TextOverflow.ellipsis)
+        // )
+      ]
+    );
+  }
 
-//     // date and registration button in card's right col
-//     Widget buildCardRight({required String month, required String day}){
-//       return Column(
-//       crossAxisAlignment: CrossAxisAlignment.center, //left-aligned
-//       children: [
-//         Container(
-//           child: Text(month, style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),)
-//         ),
-//         Container(
-//           padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-//           child: Text(day, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
-//           )
-//         ),
-//         RegisterButton()
-//       ]
-//     );
-//   }
+    // date and registration button in card's right col
+    Widget buildCardRight({required String month, required String day}){
+      return Column(
+      crossAxisAlignment: CrossAxisAlignment.center, //left-aligned
+      children: [
+        Container(
+          child: Text(month, style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),)
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: Text(day, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+          )
+        ),
+        RegisterButton()
+      ]
+    );
+  }
 
-//   void _cardTapped(){
-//     print('card tapped');
-//   }
+  void _cardTapped(){
+    print('card tapped');
+  }
 
-// }
