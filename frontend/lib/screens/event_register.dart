227bd687c494aa7a1c959003d6200a60d473a11f
n-maido <nhi_maido@outlook.com>
2021-08-15 23:44:24 -0700

    integrate event register form with backend

--- frontend/lib/screens/event_register.dart
+++ frontend/lib/screens/event_register.dart
@@ -8,9 +8,10 @@ import '../../graphql_conf.dart';
 import '../../query_mutation.dart';
 import '../models/account.dart';
 import '../widgets/nav_drawer.dart';
+import 'display_events.dart';
+import 'event_button_mode.dart';
 import 'event_confirmation.dart';
 
-String username = "";
 String name = "";
 String email = "";
 
@@ -96,13 +97,56 @@ class _EventRegisterState extends State<EventRegister> {
             SizedBox(
               height: 15,
             ),
-            
-            ElevatedButton(
-              onPressed: _registerPressed,
-              //style: ButtonStyle(padding: EdgeInsets.all(0.0), ),
-              child: const Text('Register', style: TextStyle(fontSize: 11)),
-              style: ElevatedButton.styleFrom(primary: Palette.highlight_1),
-            ),
+            Mutation(
+              options: MutationOptions(
+                documentNode: gql(registerForEvent),
+                onCompleted: (dynamic resultData){
+                  if(resultData?.isEmpty ?? true){
+                    print('null data');
+                  }
+                  else{
+                    print('nonnull data');
+                    var data = resultData.data["addAttendees"];
+
+                    setState(() {
+                      
+                    });
+
+                    print("mutation added: ${data}");
+                  }
+                },
+                onError: (err) {
+                  print(err.graphqlErrors);
+                },
+              ), 
+              builder: (RunMutation runMutation, QueryResult result){
+                // next & submit button
+                return ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()){
+                      debugPrint('All fields entered.');
+                      print('submit pressed');
+                      runMutation({
+                          "eventId": widget.eventID,
+                          "userId": "1",
+                      });
+                      Navigator.push(context, PageRouteBuilder(
+                        opaque: false,
+                        transitionDuration: Duration.zero,
+                        pageBuilder: (BuildContext context, _, __) {
+                          return DisplayEvents(screen: ScreenType.Attending, mode: EventButtonMode.Cancel);
+                        }
+                      )
+                      );
+                    }
+                    
+                  },
+                  //style: ButtonStyle(padding: EdgeInsets.all(0.0), ),
+                  child: const Text('Register', style: TextStyle(fontSize: 11)),
+                  style: ElevatedButton.styleFrom(primary: Palette.highlight_1),
+                );
+                },
+              ),
           ],
         ),
       ),
@@ -160,29 +204,6 @@ class _buildForm extends StatelessWidget {
             ),
             SizedBox(
               height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              // username
-              child: TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Username required';
-                  } else if (value.length < 3) {
-                    return 'Username must be at least 3 characters long.';
-                  }
-                  else{
-                    username = value;
-                    return null;
-                  }
-                },
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  hintText: 'Enter your Username',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                ),
-              ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
