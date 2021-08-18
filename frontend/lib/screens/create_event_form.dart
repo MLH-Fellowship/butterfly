36701409fbe46fd6c8577b279c77358e7f56253e
YenyuHuang <yenyu.huang99@gmail.com>
2021-08-18 13:28:14 -0400

    added showDatePicker

--- frontend/lib/screens/create_event_form.dart
+++ frontend/lib/screens/create_event_form.dart
@@ -8,6 +8,10 @@ import '../../graphql_conf.dart';
 import '../../query_mutation.dart';
 import '../models/account.dart';
 import '../widgets/nav_drawer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:dropdown_date_picker/dropdown_date_picker.dart';
+import 'dart:async';
+import 'package:sqflite/sqflite.dart';
 
 String eventName = "";
 String date = "";
@@ -20,56 +24,30 @@ String description = "";
 class CreateEventForm extends StatefulWidget {
   final ScreenType screen;
   CreateEventForm({Key? key, required this.screen}) : super(key: key);
-
-  //final String title;
-  //final Account account; //Alert
-  //final bool isAdd; //Alert
-  //  final String updateEvent = """
-//     query updateEvent {
-//       updateEvent {
-//         name
-//         date
-//         startTime
-//         endTime
-//         tag
-//         location
-//         description
-//       }
-//     }
-//   """;
-
   @override
   State<StatefulWidget> createState() => _CreateEventFormState();
 }
 
 class _CreateEventFormState extends State<CreateEventForm> {
-  TextEditingController txtId = TextEditingController();
-  TextEditingController txtFirstName = TextEditingController();
-  TextEditingController txtLastName = TextEditingController();
-  TextEditingController txtEmail = TextEditingController();
-  // final TextEditingController txtPassword =
-  //     TextEditingController(); //passwordController
-  // final TextEditingController confirmTxtPassword =
-  //     TextEditingController(); //confirmPasswordController
-  Icon button = Icon(Icons.keyboard_return_rounded);
+  static late TextEditingController dateController = TextEditingController();
 
+  Icon button = Icon(Icons.keyboard_return_rounded);
   GraphQLConfiguration graphQLConfiguration = GraphQLConfiguration();
   QueryMutation addMutation = QueryMutation();
 
-  //final Account account;
-  //final bool isAdd;
-
   _CreateEventFormState();
   final _formKey = GlobalKey<FormState>();
-  //final _passwordController = TextEditingController();
-  //final _confirmPasswordController = TextEditingController();
 
-  // @override
-  // void dispose() {
-  //   txtPassword.dispose();
-  //   confirmTxtPassword.dispose();
-  //   super.dispose();
-  // }
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    dateController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -91,12 +69,11 @@ class _CreateEventFormState extends State<CreateEventForm> {
     return Scaffold(
       appBar: CustomBar(widget.screen, false), //display a custom title
       body: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: 10.0,
-          horizontal: 10.0,
-        ),
-        child: Column(
-          children: [
+          padding: EdgeInsets.symmetric(
+            vertical: 10.0,
+            horizontal: 10.0,
+          ),
+          child: Column(children: [
             _buildForm(formKey: _formKey),
             SizedBox(
               height: 15,
@@ -104,17 +81,17 @@ class _CreateEventFormState extends State<CreateEventForm> {
             Mutation(
               options: MutationOptions(
                 documentNode: gql(addEvent),
-                onCompleted: (dynamic resultData){
-                  if(resultData?.isEmpty ?? true){
+                onCompleted: (dynamic resultData) {
+                  if (resultData?.isEmpty ?? true) {
                     print('null data');
-                  }
-                  else{
+                  } else {
                     print('nonnull data');
                     var data = resultData.data["addEvent"];
 
                     setState(() {
                       eventName = data['name'];
                       date = data['date'];
+
                       startTime = data['startTime'];
                       endTime = data['endTime'];
                       tag = data['tag'];
@@ -128,12 +105,12 @@ class _CreateEventFormState extends State<CreateEventForm> {
                 onError: (err) {
                   print(err.graphqlErrors);
                 },
-              ), 
-              builder: (RunMutation runMutation, QueryResult result){
+              ),
+              builder: (RunMutation runMutation, QueryResult result) {
                 // next & submit button
                 return ElevatedButton(
                   onPressed: () {
-                    if (_formKey.currentState!.validate()){
+                    if (_formKey.currentState!.validate()) {
                       debugPrint('All fields entered.');
                       print('submit pressed');
                       runMutation({
@@ -143,27 +120,24 @@ class _CreateEventFormState extends State<CreateEventForm> {
                           "startTime": startTime,
                           "endTime": endTime,
                           "tag": tag,
-                          "user": "1", 
+                          "user": "1",
                           "location": location,
                           "description": description
                         }
                       });
                     }
-                    
                   },
                   //style: ButtonStyle(padding: EdgeInsets.all(0.0), ),
                   child: const Text('Submit', style: TextStyle(fontSize: 11)),
                   style: ElevatedButton.styleFrom(primary: Palette.highlight_1),
                 );
-                },
-              ),
-              
-            ]
+              },
+            ),
+          ]
               //child: Icon(Icons.keyboard_return_rounded),
-          )   
-        ),
-        bottomNavigationBar: BottomNav(screen: ScreenType.Create),
-      );
+              )),
+      bottomNavigationBar: BottomNav(screen: ScreenType.Create),
+    );
   }
 
   void _submitPressed() {
@@ -174,13 +148,57 @@ class _CreateEventFormState extends State<CreateEventForm> {
 }
 
 class _buildForm extends StatelessWidget {
-  const _buildForm({
+  DateTime currentDate = DateTime.now();
+  void setState(Null Function() param0) {}
+  _buildForm({
     Key? key,
     required GlobalKey<FormState> formKey,
   })  : _formKey = formKey,
         super(key: key);
 
   final GlobalKey<FormState> _formKey;
+
+  Future<void> _selectDate(BuildContext context) async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData(
+            primarySwatch: Colors.red,
+            splashColor: Colors.black,
+            textTheme: TextTheme(
+              subtitle1: TextStyle(color: Colors.black),
+              button: TextStyle(color: Colors.black),
+            ),
+            accentColor: Colors.black,
+            colorScheme: ColorScheme.light(
+                primary: Color(0xFF005B47),
+                primaryVariant: Colors.black,
+                secondaryVariant: Colors.black,
+                onSecondary: Colors.black,
+                onPrimary: Color(0xFFFAAE2B),
+                surface: Colors.black,
+                onSurface: Colors.black,
+                secondary: Colors.black),
+            dialogBackgroundColor: Colors.white,
+          ),
+          child: child ?? Text(""),
+        );
+      },
+      initialDate: currentDate,
+      firstDate: DateTime(2021),
+      lastDate: DateTime(2023),
+    );
+
+    if (pickedDate != null)
+      setState(() {
+        pickedDate = currentDate;
+        _CreateEventFormState.dateController.text = pickedDate.toString();
+        date = _CreateEventFormState.dateController.text;
+      });
+    _CreateEventFormState.dateController.text = pickedDate.toString();
+    date = _CreateEventFormState.dateController.text;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -197,8 +215,7 @@ class _buildForm extends StatelessWidget {
                     return 'Event name required';
                   } else if (value.length < 3) {
                     return 'Event name must be at least 3 characters long.';
-                  }
-                  else{
+                  } else {
                     eventName = value;
                     return null;
                   }
@@ -214,15 +231,14 @@ class _buildForm extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextFormField(
+                controller: _CreateEventFormState.dateController,
+                onTap: () => _selectDate(context),
                 validator: (value) {
                   if (value!.isEmpty) {
                     return 'Date required';
-                  } else if (value.length < 3) {
-                    return 'Date must be at least 3 characters long.';
-                  }
-                  else{
+                  } else {
                     date = value;
-                    return null;
+                    return "Please choose a date.";
                   }
                 },
                 decoration: InputDecoration(
@@ -241,8 +257,7 @@ class _buildForm extends StatelessWidget {
                     return 'Start Time required';
                   } else if (value.length < 3) {
                     return 'Start Time must be at least 3 characters long.';
-                  }
-                  else{
+                  } else {
                     startTime = value;
                     return null;
                   }
@@ -264,8 +279,7 @@ class _buildForm extends StatelessWidget {
                     return 'End Time required';
                   } else if (value.length < 3) {
                     return 'End Time not valid.';
-                  }
-                  else{
+                  } else {
                     endTime = value;
                     return null;
                   }
@@ -287,8 +301,7 @@ class _buildForm extends StatelessWidget {
                     return 'Tag required';
                   } else if (value.length < 3) {
                     return 'Tag not valid.';
-                  }
-                  else{
+                  } else {
                     tag = value;
                     return null;
                   }
@@ -310,8 +323,7 @@ class _buildForm extends StatelessWidget {
                     return 'Location required';
                   } else if (value.length < 3) {
                     return 'Location not valid.';
-                  }
-                  else{
+                  } else {
                     endTime = value;
                     return null;
                   }
@@ -333,8 +345,7 @@ class _buildForm extends StatelessWidget {
                     return 'Description required';
                   } else if (value.length < 3) {
                     return 'Description not valid.';
-                  }
-                  else{
+                  } else {
                     endTime = value;
                     return null;
                   }
