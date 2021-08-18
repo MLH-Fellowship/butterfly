3e8f2e400c5f121d11e742a8c02abc88c1013945
manlalaro1 <odonnellp46@gmail.com>
2021-08-18 00:32:31 -0400

    added unique attribute to email field

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
 
     def __str__(self):
