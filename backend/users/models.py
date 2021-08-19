78ca96b327f525c2e857cd6407ae3c83a6bd5711
manlalaro1 <odonnellp46@gmail.com>
2021-08-19 16:29:47 -0400

    added unit tests for user and event models, deleted default values in models

--- backend/users/models.py
+++ backend/users/models.py
@@ -10,13 +10,13 @@ class User(models.Model):
 
 class Event(models.Model):
     name = models.CharField(max_length=100)
-    date = models.DateField(default="2021-08-20")
-    startTime = models.TimeField(default="14:30:59")
-    endTime = models.TimeField(default="14:30:59")
+    date = models.DateField()
+    startTime = models.TimeField()
+    endTime = models.TimeField()
     tag = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1, related_name="user")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
     location = models.CharField(max_length=100)
-    description = models.CharField(max_length=100, default="Event")
+    description = models.CharField(max_length=100, default="Event description")
     attendees = models.ManyToManyField(User, related_name="attending_users")
     
     def __str__(self):
--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,70 @@
 from django.test import TestCase
+import json
+from .models import User, Event
 
-# Create your tests here.
+# Test user model string representation
+class UserTestCase(TestCase):
+    def test_string_representation(self):
+        user = User.objects.create(
+            name="Test User", email="testuser@gmail.com", password="testpassword123")
+            
+        self.assertEqual(str(user), user.name)
+        self.assertEqual("testuser@gmail.com", user.email)
+        self.assertEqual("testpassword123", user.password)
+
+
+# Test event model string representation, user to user relation, and attendee to user relation
+class EventTestCase(TestCase):
+    def test_string_representation(self):
+        user = User.objects.create(
+            name="Test User", email="testuser@gmail.com", password="testpassword123")
+        event = Event.objects.create( 
+            name="Test Event", date="2021-08-20", startTime="0:0:59", endTime="1:30:59", tag="Test tag", description="This is a test paragraph. This paragraph is longer than normal text", user=user)
+
+        self.assertEqual(str(event), event.name)
+        self.assertEqual("2021-08-20", event.date)
+        self.assertEqual("0:0:59", event.startTime)
+        self.assertEqual("1:30:59", event.endTime)
+        self.assertEqual("Test tag", event.tag)
+        self.assertEqual("This is a test paragraph. This paragraph is longer than normal text", event.description)
+
+
+    def test_user_relation(self):
+        user = User.objects.create(
+            name="Test User", email="testuser@gmail.com", password="testpassword123")
+        event = Event.objects.create(
+            name="Test Event", date="2021-08-20", startTime="0:0:59", endTime="1:30:59", tag="Test tag", user=user)
+
+        self.assertEquals(user.id, event.user.id)
+        self.assertEqual(str(user), event.user.name)
+        self.assertEqual("testuser@gmail.com", event.user.email)
+        self.assertEqual("testpassword123", event.user.password)
+    
+    def test_attendee_relation(self):
+        user1 = User.objects.create(
+            name="Test User 1", email="testuser1@gmail.com", password="testpassword123")
+        user2 = User.objects.create(
+            name="Test User 2", email="testuser2@gmail.com", password="testpassword1234")
+        user3 = User.objects.create(
+            name="Test User 3", email="testuser3@gmail.com", password="testpassword12345")
+        event = Event.objects.create(
+            name="Test Event", date="2021-08-20", startTime="0:0:59", endTime="1:30:59", tag="Test tag", user=user1)
+        
+        event.attendees.add(user1)
+        event.attendees.add(user2)
+        event.attendees.add(user3)
+
+        self.assertEqual(user1.id, event.attendees.get(id=user1.id).id)
+        self.assertEqual(str(user1), event.attendees.get(id=user1.id).name)
+        self.assertEqual("testuser1@gmail.com", event.attendees.get(id=user1.id).email)
+        self.assertEqual("testpassword123", event.attendees.get(id=user1.id).password)
+
+        self.assertEqual(user2.id, event.attendees.get(id=user2.id).id)
+        self.assertEqual(str(user2), event.attendees.get(id=user2.id).name)
+        self.assertEqual("testuser2@gmail.com", event.attendees.get(id=user2.id).email)
+        self.assertEqual("testpassword1234", event.attendees.get(id=user2.id).password)
+
+        self.assertEqual(user3.id, event.attendees.get(id=user3.id).id)
+        self.assertEqual(str(user3), event.attendees.get(id=user3.id).name)
+        self.assertEqual("testuser3@gmail.com", event.attendees.get(id=user3.id).email)
+        self.assertEqual("testpassword12345", event.attendees.get(id=user3.id).password)
