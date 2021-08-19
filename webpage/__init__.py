ec0780b8d560a55ddbe95aba15b0a03c9409f681
n-maido <nhi_maido@outlook.com>
2021-08-18 19:33:33 -0700

    test deployment

--- webpage/__init__.py
+++ webpage/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, request, redirect, json
 import os
 import json
 
-
 app = Flask(__name__)
 
 # Create URL routes
@@ -10,7 +9,6 @@ app = Flask(__name__)
 def home():
 
     return render_template("home.html")
-
 
 if __name__ == "__main__":
     # rid (port="5002") within run function
