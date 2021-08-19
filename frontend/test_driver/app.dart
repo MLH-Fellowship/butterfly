32cc38b2c6febb395366abdfc8d648e788ed0575
YenyuHuang <yenyu.huang99@gmail.com>
2021-08-19 01:58:46 -0400

    trying to get integration testing started with flutter_driver but dependecies are conflicted

---
+++ frontend/test_driver/app.dart
@@ -0,0 +1,12 @@
+// this test “drive” the app and record performance profiles from a test suite
+import 'package:flutter_driver/driver_extension.dart';
+import '../lib/main.dart' as app;
+
+void main() {
+  // This line enables the extension.
+  enableFlutterDriverExtension();
+
+  // Call the `main()` function of the app, or call `runApp` with
+  // any widget you are interested in testing.
+  app.main();
+}
